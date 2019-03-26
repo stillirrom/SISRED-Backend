2c64cb91b613df1a937efd3e6c7e3233a3b9bcb9
Julián Picón <julian.picon@hotmail.com>
2019-03-25 23:29:50 -0500

    Ajuste JSON de respuesta

--- sisred/settings.py
+++ sisred/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'sisred.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbsisred',
+        'USER': 'usdbsisred',
+        'PASSWORD': 'sisred',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
