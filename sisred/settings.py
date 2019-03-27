54e4035f0ad7825b0cdc0db65fa1eb37ac5f6e87
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-03-27 16:32:46 -0500

    config heroku

--- sisred/settings.py
+++ sisred/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+#Heroku configurations
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
