ecceed84aef457aea642b594bb3addcc3e3dde7e
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-03-27 14:53:42 -0500

    adding estado service

--- sisred_app/serializer.py
+++ sisred_app/serializer.py
@@ -1,8 +1,13 @@
-from .models import  Recurso
+from .models import Recurso, ProyectoRED, Estado
 from rest_framework import  serializers
 
 class RecursoSerializer(serializers.ModelSerializer):
     class Meta:
         model=Recurso
         fields=('nombre','archivo','thumbnail','fecha_creacion','fecha_ultima_modificacion','tipo','descripcion','metadata','autor','usuario_ultima_modificacion')
+
+class EstadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Estado
+        fields=('id','nombre_estado')
 
