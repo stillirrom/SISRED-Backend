ec1e4ab39610eb381a4553134c732b7960764fd3
deisy <dt.macias@uniandes.edu.co>
2019-03-28 00:15:23 -0500

    Agrega archivo para las urls de sisred_app

---
+++ sisred_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+
+app_name = 'sisred_app'
+
+urlpatterns = [
+    path('users/', views.get_all_users, name='allUsers'),
+    path('users/<int:id>/', views.get_user_id_view, name='getUserId'),
+    path('users/add/', views.add_user_view, name='addUser'),
+    path('users/update/<int:id>/', views.update_user_view, name='updateUser'),
+]
