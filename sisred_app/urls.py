0917d96c50478f307981b192a919a90c4c0e25db
jh.rincon <jh.rincon@uniandes.edu.co>
2019-03-28 12:56:19 -0500

    Status ajustes al file de urls (VERDE)

--- sisred_app/urls.py
+++ sisred_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('users/<int:id>/', views.get_user_id_view, name='getUserId'),
     path('users/add/', views.add_user_view, name='addUser'),
     path('users/update/<int:id>/', views.update_user_view, name='updateUser'),
-    path('reds/relacionados/<int:id>', views.get_reds_relacionados, name='reds_relacionados')
+    path('reds/relacionados/<int:id>', views.get_reds_relacionados, name='reds_relacionados'),
+    path('getRecurso/', views.getRecurso, name='getRecurso'),
+    path('getRedDet/', views.getRedDet, name='getRedDet'),
 
 ]
