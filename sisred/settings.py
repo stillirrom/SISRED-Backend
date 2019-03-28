114502fa007821ee192c59a9436e1cc3b12f645a
jh.rincon <jh.rincon@uniandes.edu.co>
2019-03-28 14:55:25 -0500

    Configuracion de database

--- sisred/settings.py
+++ sisred/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sisred.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbsisred',
+        'USER': 'usdbsisred',
+        'PASSWORD': 'zaq123edc',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
