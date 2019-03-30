6e61a706dbf6e8dec175d6776db167c24e3bcd39
Camilo Sánchez <ca.sanchez38@uniandes.edu.co>
2019-03-29 21:06:52 -0500

    Setup Deplyment Heroku

--- sisred/settings.py
+++ sisred/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
