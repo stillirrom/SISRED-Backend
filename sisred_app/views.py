c31bcb0ff835cb8d4d0e9561c784df560a13de3e
fjpging8908 <fjpging@gmail.com>
2019-03-29 20:48:51 -0500

    Ajustes a la vista para que la vista recurso_addget permita modificar un recurso se valida si el objeto recibo en el POST existe, de ser asi, actualiza el Nombre, Descripción, Usuario_ultima_modificacion y la fecha de modificacion

--- sisred_app/views.py
+++ sisred_app/views.py
@@ -3,10 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 
 from rest_framework.response import Response
+import datetime
 
 
-
-from sisred_app.models import Recurso, RED
+from sisred_app.models import Recurso, RED, Perfil
 from sisred_app.serializer import RecursoSerializer, FaseSerializer
 
 
@@ -39,9 +39,22 @@ def recurso_addget(request,id):
     elif request.method == 'POST':
         serializer = RecursoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            ItemRecurso = Recurso.objects.filter(id=id).first()
+            if (ItemRecurso!=None):
+                ItemRecurso.nombre=request.data.get("nombre")
+                ItemRecurso.descripcion=request.data.get("descripcion")
+
+                Per=Perfil.objects.filter(id=int(request.data.get("usuario_ultima_modificacion")))
+                #if (Per!=None):
+                    #ItemRecurso.usuario_ultima_modificacion=Per
+                ItemRecurso.fecha_ultima_modificacion=datetime.datetime.now()
+                ItemRecurso.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(['GET', 'POST'])
 def fase_byid(request,id):
     if request.method == 'GET':
