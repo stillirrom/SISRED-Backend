439c1263267658d2978b7fd998ae844d5d9dbe9e
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-03-31 19:05:29 -0500

    servicio para reds por persona

--- sisred_app/urls.py
+++ sisred_app/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('getProyectosRED/', views.getProyectosRED),
+    path('getRecurso/', views.getRecurso),
+    path('getRED/', views.getRED),
+    path('asignaciones/', views.getAsignaciones),
+]
--- sisred_app/views.py
+++ sisred_app/views.py
@@ -1,16 +1,62 @@
-from django.core import serializers
-from django.http import HttpResponse
-from sisred_app.models import ProyectoRED, Recurso, RED
+from django.core.serializers import *
+from rest_framework import serializers
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from sisred_app.models import ProyectoRED, Recurso, RED, RolAsignado, Perfil, Rol
+from django.contrib.auth.models import User
 
-
+@csrf_exempt
 def getProyectosRED(request):
     vLstObjects = list(ProyectoRED.objects.all())
-    return HttpResponse(serializers.serialize('json', vLstObjects), content_type="application/json")
+    return HttpResponse(serialize('json', vLstObjects), content_type="application/json")
 
+@csrf_exempt
 def getRecurso(request):
     vLstObjects = list(Recurso.objects.all())
-    return HttpResponse(serializers.serialize('json', vLstObjects), content_type="application/json")
+    return HttpResponse(serialize('json', vLstObjects), content_type="application/json")
 
+@csrf_exempt
 def getRED(request):
     vLstObjects = list(RED.objects.all())
-    return HttpResponse(serializers.serialize('json', vLstObjects), content_type="application/json")
+    return HttpResponse(serialize('json', vLstObjects), content_type="application/json")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name')
+
+
+class PerfilSerializer(serializers.ModelSerializer):
+    usuario = UserSerializer()
+    class Meta:
+        model = Perfil
+        fields = '__all__'
+
+
+class RedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RED
+        fields = '__all__'
+
+
+class RolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rol
+        fields = '__all__'
+
+
+class RolAsignadoSerializer(serializers.ModelSerializer):
+    red = RedSerializer()
+    usuario = PerfilSerializer()
+    rol = RolSerializer()
+    class Meta:
+        model = RolAsignado
+        fields = ('fecha_inicio', 'fecha_fin', 'red', 'rol', 'usuario')
+
+
+@csrf_exempt
+def getAsignaciones(request):
+    data = list(RolAsignado.objects.all())
+    serializer = RolAsignadoSerializer(data, many=True)
+    return JsonResponse({'context': serializer.data}, safe=True)
