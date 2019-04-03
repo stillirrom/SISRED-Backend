95e1a594082ea590f7f9475d0d99a1952e7f7c69
vivasoft <vargaspintoadriana@gmail.com>
2019-04-02 21:27:58 -0500

    Fix urls

--- sisred_app/urls.py
+++ sisred_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from sisred_app.views import views_equipo3
+from sisred_app.views import views_equipo3, views_equipo1
 
 urlpatterns = [
 
-    path('recurso_list/', views.recurso_list, name='recurso_list'),
-    path('recurso_get/<int:id>', views.recurso_get, name='recurso_get'),
-    path('recurso_put/', views.recurso_put, name='recurso_put'),
-    path('recurso_post/', views.recurso_post, name='recurso_post'),
-    path('fase_byid', views.fase_byid, name='fase_byid'),
+    path('recurso_list/', views_equipo1.recurso_list, name='recurso_list'),
+    path('recurso_get/<int:id>', views_equipo1.recurso_get, name='recurso_get'),
+    path('recurso_put/', views_equipo1.recurso_put, name='recurso_put'),
+    path('recurso_post/', views_equipo1.recurso_post, name='recurso_post'),
+    path('fase_byid', views_equipo1.fase_byid, name='fase_byid'),
 
 
     path('post_proyecto_red/', views_equipo3.post_proyecto_red, name='agregar_proyecto_red'),
