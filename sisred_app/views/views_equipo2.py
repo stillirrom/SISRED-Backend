c1227d23f9aa1241abe3ebb0916a257559519663
Juan Camilo Piza Jerez <juan.piza@globant.com>
2019-04-12 13:48:53 -0500

    corrigiendo identacion

--- sisred_app/views/views_equipo2.py
+++ sisred_app/views/views_equipo2.py
@@ -34,7 +34,7 @@ def marcarVersion(request,id):
 
         version.es_final = True
         version.save()
-	    return JsonResponse(str(id), safe=False)
+        return JsonResponse(str(id), safe=False)
     return HttpResponseNotFound()
 
 
