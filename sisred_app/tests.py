26a91e5f8a707b460077f4a6fc0737764e1470c1
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-04-14 11:42:41 -0500

    Test para login con id conectate (Rojo)

--- sisred_app/tests.py
+++ sisred_app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from sisred_app.models import User
+from sisred_app.models import User, Perfil
 
 import json
 from django.contrib import auth
@@ -9,13 +9,19 @@ from django.contrib import auth
 class loginTestCase(TestCase):
     def test_login(self):
         username = "gl.pinto10@uniandes.edu.co"
+        first_name = "first_name"
+        last_name = "last_name"
         password = "Test12345"
-
-        user = User(first_name="Test", last_name="Test", email=username, username=username, password=password)
-        user.save()
+        id_conectate = "1"
+        numero_identificacion = "123455"
+        user_model = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=username)
+        user_profile = Perfil.objects.create(usuario=user_model, id_conectate=id_conectate,
+                                             numero_identificacion=numero_identificacion, estado=1)
+        user_profile.save()
 
         response = self.client.post('/api/login/', json.dumps({"username": username, "password": password}),
                                     content_type='application/json')
         current_data = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(current_data.token)
+        self.assertIsNotNone(current_data['token'])
+        self.assertEqual(id_conectate, current_data['idConectate'])
