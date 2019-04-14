fea0b8bfe702a7055f1de2f16fb84ae260369db9
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-04-14 11:17:49 -0500

    Test para login (Rojo)

--- sisred_app/tests.py
+++ sisred_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from sisred_app.models import User
+
+import json
+from django.contrib import auth
 
 # Create your tests here.
+
+class loginTestCase(TestCase):
+    def test_login(self):
+        username = "gl.pinto10@uniandes.edu.co"
+        password = "Test12345"
+
+        user = User(first_name="Test", last_name="Test", email=username, username=username, password=password)
+        user.save()
+
+        response = self.client.post('/api/login/', json.dumps({"username": username, "password": password}),
+                                    content_type='application/json')
+        current_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(current_data.token)
