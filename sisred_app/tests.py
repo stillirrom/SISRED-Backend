bf82a5652eb6afcf80c02f0397ce19e9db70ed68
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2019-04-25 21:01:24 -0500

    Status respuesta comentario pdf (ROJO)

--- sisred_app/tests.py
+++ sisred_app/tests.py
@@ -41,4 +41,52 @@ class SisredTestCase(TestCase):
 
         self.assertEqual(current_data[0]['listo'], True)
 
+    def test_get_comentario_pdf(self):
+        user_model1 = User.objects.create_user(username='test1', password='kd8wke-DE34', first_name='test1',
+                                               last_name='test1', email='test1@test.com')
+
+        profile1 = Perfil.objects.create(usuario=user_model1, numero_identificacion="1100960499", estado="1")
+        red = RED.objects.create( id_conectate="S0001", nombre="null", nombre_corto="null", descripcion="1 video", fecha_inicio="2019-12-31", fecha_cierre="2019-12-31", fecha_creacion="2019-12-31", porcentaje_avance="0", tipo="Sin definir", solicitante="PR0011(Sandra)", horas_estimadas="0", horas_trabajadas="0", proyecto_conectate_id="1")
+        version = Version.objects.create(numero=1, archivo="prueba", red=red)
+        recurso = Recurso.objects.create(nombre="test",archivo="test",thumbnail="test",tipo="test",descripcion="test",autor=profile1,usuario_ultima_modificacion=profile1)
+        comentario = Comentario.objects.create(usuario=profile1, version=version, recurso=recurso,  contenido="comentario de prueba")
+        comMultimedia = ComentarioMultimedia.objects.create(x1=121, x2=121, y1=121, y2=121, comentario=comentario)
+        comPDF= ComentarioPDF.objects.create(height=123,width=123,comentario_multimedia=comMultimedia)
+        id = recurso.id
+
+        response = self.client.get('/api/comentarios/pdf/'+str(id))
+        current_data = json.loads(response.content)
+        print(current_data)
+
+        self.assertEqual(current_data[0]['comentario'], "comentario de prueba")
+
+    def test_post_comentario_pdf(self):
+        user_model1 = User.objects.create_user(username='test1', password='kd8wke-DE34', first_name='test1',
+                                               last_name='test1', email='test1@test.com')
+
+        profile1 = Perfil.objects.create(usuario=user_model1, numero_identificacion="1100960499", estado="1")
+        red = RED.objects.create(id_conectate="S0001", nombre="null", nombre_corto="null", descripcion="1 video",
+                                 fecha_inicio="2019-12-31", fecha_cierre="2019-12-31", fecha_creacion="2019-12-31",
+                                 porcentaje_avance="0", tipo="Sin definir", solicitante="PR0011(Sandra)",
+                                 horas_estimadas="0", horas_trabajadas="0", proyecto_conectate_id="1")
+        version = Version.objects.create(numero=1, archivo="prueba", red=red)
+        recurso = Recurso.objects.create(nombre="test", archivo="test", thumbnail="test", tipo="test",
+                                         descripcion="test", autor=profile1, usuario_ultima_modificacion=profile1)
+        data={"rutaPdf": "http://www.orimi.com/pdf-test.pdf",
+              "Usuario":1,
+              "Version":1,
+              "recurso":1,
+              "coordenadas":{
+                  "x1": 142432,
+                  "x2": 134234,
+                  "y1": 142432,
+                  "y2": 142432,
+                  "width": 200,
+                  "height": 300},
+              "comentario": "contenido"}
+        response = self.client.post('/api/comentarios/pdf/',data)
+        current_data = json.loads(response.content)
+        print(current_data)
+
+        self.assertEqual(current_data[0]['comentario'], "contenido")
 
