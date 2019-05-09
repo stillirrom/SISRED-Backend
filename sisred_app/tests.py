2732a32202621c9b3e9e4e48b1f2e993c780a420
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-05-08 23:59:00 -0500

    Consultar usuarios asignados a un red (ROJO)

--- sisred_app/tests.py
+++ sisred_app/tests.py
@@ -1182,6 +1182,30 @@ class sisRedTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+
+    def test_get_historico_asignados_red(self):
+
+        fecha = datetime.datetime.now()
+        proyecto = ProyectoConectate.objects.create(id=1, fecha_inicio=fecha, fecha_fin=fecha)
+        red = RED.objects.create(id=1, nombre='pruebaRED', descripcion='prueba',
+                                 tipo='prueba', solicitante='prueba', proyecto_conectate=proyecto)
+        user1 = User.objects.create_user(username='test', password='sihdfnssejkhfse', email='test@test.com', first_name='Nathalia')
+        user2 = User.objects.create_user(username='test2', password='sihdfnssejkhfse', email='test@test.com', first_name='Fabian')
+        perfil1 = Perfil.objects.create(id_conectate='1', usuario=user1, estado=1)
+        perfil2 = Perfil.objects.create(id_conectate='2', usuario=user2, estado=1)
+        rol1 = Rol.objects.create(id_conectate='1', nombre='rol1')
+        rol2 = Rol.objects.create(id_conectate='2', nombre='rol2')
+        RolAsignado.objects.create(id_conectate='1', estado=1, red=red, rol=rol1, usuario=perfil1)
+        RolAsignado.objects.create(id_conectate='2', estado=1, red=red, rol=rol2, usuario=perfil2)
+
+        url = '/api/getHistoricoAsignadosRed/' + str(red.id) + '/'
+
+        response = self.client.get(url, format='json')
+        usuarios = json.loads(response.content)
+        self.assertEqual(len(usuarios), 2)
+        self.assertEqual(usuarios[0]['first_name'], 'Nathalia')
+        self.assertEqual(usuarios[1]['first_name'], 'Fabian')
+
 class RR02TestCase(TestCase):
 
     def test_get_version(self):
